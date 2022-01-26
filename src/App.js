bce99f16db178490a80f6a5df97d01f4f3ccae74
Kumar Sanjay <sanjayism@gmail.com>
2022-01-26 23:34:10 +0530

    lession 10 completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import TextForm from "./components/TextForm";
 import Header from "./components/Header";
+import About from "./components/About";
 
 
 function App() {
   return (
     <>
     <Header  />
-    <div className="container"><TextForm heading="Enter the Text to Analyse"/></div>
+    <div className="container">
+      <TextForm heading="Enter the Text to Analyse"/>
+      <About />
+    </div>
     
+     
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ import React,{useState} from 'react';
 
 const TextForm = (props) => {
   const [text,setText]=useState('Enter text here')
+  const [myStyle,setMyStyle]=useState({text:'normal'})
   //text="new text"; //wrong way to change the state of text variable.
   //setText("new text"); //right way to change the state of text variable.
 
@@ -15,16 +16,72 @@ const TextForm = (props) => {
   const handleUpClick=()=>{
     //console.log('Upper case Button Clicked')
     let newText=text.toUpperCase()
-    setText(newText)
-   
+    setText(newText) 
   }
+  const handleLoClick=()=>{
+    //console.log('Upper case Button Clicked')
+    let newText=text.toLowerCase()
+    setText(newText) 
+  }
+
+  const handleDelClick=()=>{
+     setText("") 
+  }
+
+  const handleCopyClick =()=>{
+    navigator.clipboard.writeText(text)
+  }
+  const handleCutClick =()=>{
+    navigator.clipboard.cut(text)
+  }
+  const handlePasteClick =()=>{
+    // const text1=[...text,setText(text)]
+    // console.log(text1)
+    // navigator.clipboard.readText(text1)
+  }
+
+  const handleRemoveExtraSpace =()=>{
+    const text1=text.split(/[ ]+/)
+    setText(text1.join(" "))
+  }
+  const handleReverseString=()=>{ 
+    if (text==='')
+    {alert("please first enter text in textbox");}
+    else{
+    let resultOfReverveString=text.split(' ').reverse().toString().replaceAll(',','');
+    setText(resultOfReverveString);
+    }
+}
+const ToBold = () => {
+  
+  setMyStyle({text:'bold'});
+}
   return <>
+  <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <label htmlFor="myBox" className="form-label">Example textarea</label>
-            <textarea className="form-control" id="myBox" rows={3} value={text} onChange={handleOnChange}/>
+            <textarea className="form-control" id="myBox" rows={3} value={text} onChange={handleOnChange} style={myStyle}/>
         </div>
-        <div className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</div>
+        <div className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</div>
+        <div className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</div>
+        <div className="btn btn-primary mx-1" onClick={handleDelClick}>Clear Text</div>
+        <div className="btn btn-primary mx-1" onClick={handleCutClick}>Cut Text</div>
+        <div className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</div>
+        <div className="btn btn-primary mx-1" onClick={handlePasteClick}>Paste Text</div>
+        <div className="btn btn-primary mx-1" onClick={handleReverseString}>Reverse words</div>
+        <div className="btn btn-primary mx-1" onClick={handleRemoveExtraSpace}>Remove Extra Space</div>
+        <div className="btn btn-primary mx-1" onClick={ToBold}>Text Bold</div>
+
+  </div> 
+  <div className="container">
+    <h2>Your text summery</h2>
+    <p>{text.split(".").length} sentances.</p>
+    <p>{text.split("").length} words and {text.length} charecters.</p>
+    <p>{0.008*text.split("").length} minutes read</p>
+    <h3>Preview</h3>
+    <p>{text}</p>
+  </div>
   </>;
 };
 
