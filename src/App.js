48474c3e69f964c40d09c97488aee7f64dbb83ce
Kumar Sanjay <sanjayism@gmail.com>
2022-01-27 21:20:41 +0530

    Router lession 16 complited

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import TextForm from "./components/TextForm";
 import Header from "./components/Header";
 import About from "./components/About";
-
+import { useState } from "react";
+import Alert from "./components/Alert";
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
+  const [mode,setMode]=useState('light')
+  const [alert,setAlert]=useState(null)
+
+  const showAlert=(message,type)=>{
+     setAlert({
+       msg:message,
+       type:type
+     })
+     setTimeout(()=>{
+       setAlert(null)
+       },1500)
+  }
+  const toggleMode =()=>{
+    if(mode==='dark'){
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showAlert('Light Mode has been Enabled',"success")
+      document.title='Text Utility-Light Mode'
+    }else{
+      setMode('dark')
+      document.body.style.backgroundColor='gray'
+      showAlert('Dark Mode has been Enabled',"success")
+      document.title='Text Utility-Dark Mode'
+    }
+  }
   return (
     <>
-    <Header  />
+    <Header  mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container">
-      <TextForm heading="Enter the Text to Analyse"/>
+     
+    <Routes>
+        <Route path="/" element={ <TextForm heading="Enter the Text to Analyse" mode={mode} showAlert={showAlert}/> } />
+        <Route path="/about" element={ <About /> } />
+        
+      </Routes>
       <About />
     </div>
     
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,7 +3,7 @@ import React,{useState} from 'react';
 
 const TextForm = (props) => {
   const [text,setText]=useState('Enter text here')
-  const [myStyle,setMyStyle]=useState({text:'normal'})
+  
   //text="new text"; //wrong way to change the state of text variable.
   //setText("new text"); //right way to change the state of text variable.
 
@@ -17,22 +17,27 @@ const TextForm = (props) => {
     //console.log('Upper case Button Clicked')
     let newText=text.toUpperCase()
     setText(newText) 
+    props.showAlert("Text has been converted in Uppercase",'success')
   }
   const handleLoClick=()=>{
     //console.log('Upper case Button Clicked')
     let newText=text.toLowerCase()
     setText(newText) 
+    props.showAlert("Text has been converted in Lowercase",'success')
   }
 
   const handleDelClick=()=>{
      setText("") 
+     props.showAlert("Text has been Deleted",'success')
   }
 
   const handleCopyClick =()=>{
     navigator.clipboard.writeText(text)
+    props.showAlert("Text has been copied in clipboard",'success')
   }
   const handleCutClick =()=>{
     navigator.clipboard.cut(text)
+    props.showAlert("Text has been cut in clipboard",'success')
   }
   const handlePasteClick =()=>{
     // const text1=[...text,setText(text)]
@@ -43,6 +48,7 @@ const TextForm = (props) => {
   const handleRemoveExtraSpace =()=>{
     const text1=text.split(/[ ]+/)
     setText(text1.join(" "))
+    props.showAlert("Extra spaces from Text has been removed",'success')
   }
   const handleReverseString=()=>{ 
     if (text==='')
@@ -50,37 +56,35 @@ const TextForm = (props) => {
     else{
     let resultOfReverveString=text.split(' ').reverse().toString().replaceAll(',','');
     setText(resultOfReverveString);
+    props.showAlert("Word of the Text has been reversed",'success')
     }
 }
-const ToBold = () => {
-  
-  setMyStyle({text:'bold'});
-}
+
   return <>
-  <div className="container">
+  <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <label htmlFor="myBox" className="form-label">Example textarea</label>
-            <textarea className="form-control" id="myBox" rows={3} value={text} onChange={handleOnChange} style={myStyle}/>
+            <textarea className="form-control" id="myBox" rows={3} value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'gray':'light', color:props.mode==='dark'?'white':'black'}}/>
         </div>
-        <div className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</div>
-        <div className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</div>
-        <div className="btn btn-primary mx-1" onClick={handleDelClick}>Clear Text</div>
-        <div className="btn btn-primary mx-1" onClick={handleCutClick}>Cut Text</div>
-        <div className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</div>
-        <div className="btn btn-primary mx-1" onClick={handlePasteClick}>Paste Text</div>
-        <div className="btn btn-primary mx-1" onClick={handleReverseString}>Reverse words</div>
-        <div className="btn btn-primary mx-1" onClick={handleRemoveExtraSpace}>Remove Extra Space</div>
-        <div className="btn btn-primary mx-1" onClick={ToBold}>Text Bold</div>
+        <div className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</div>
+        <div className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</div>
+        <div className="btn btn-primary mx-1 my-1" onClick={handleDelClick}>Clear Text</div>
+        {/* <div className="btn btn-primary mx-1" onClick={handleCutClick}>Cut Text</div> */}
+        <div className="btn btn-primary mx-1 my-1" onClick={handleCopyClick}>Copy Text</div>
+        <div className="btn btn-primary mx-1 my-1" onClick={handlePasteClick}>Paste Text</div>
+        <div className="btn btn-primary mx-1 my-1" onClick={handleReverseString}>Reverse words</div>
+        <div className="btn btn-primary mx-1 my-1" onClick={handleRemoveExtraSpace}>Remove Extra Space</div>
+        
 
   </div> 
-  <div className="container">
+  <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
     <h2>Your text summery</h2>
     <p>{text.split(".").length} sentances.</p>
     <p>{text.split("").length} words and {text.length} charecters.</p>
     <p>{0.008*text.split("").length} minutes read</p>
     <h3>Preview</h3>
-    <p>{text}</p>
+    <p>{(text.length>0?text:"Enter Some Text to Get Preview")}</p>
   </div>
   </>;
 };
